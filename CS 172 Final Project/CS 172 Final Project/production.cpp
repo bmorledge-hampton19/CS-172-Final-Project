8a29986d5eb6f5d8f193f0c9dbb8bc7a6e5b4cd9
NIGHTINGALE\Eben Schumann <humanphyti@gmail.com>
2016-12-04 14:54:35 -0800

    Filled upgrade.h and upgrade.cpp I forget exactly what two of the functions were supposed to do so I commented that on them.

---
+++ CS 172 Final Project/CS 172 Final Project/production.cpp
@@ -0,0 +1,31 @@
+#include "production.h"
+
+Upgrade::Upgrade(long int upgradeCppmAdditive, double upgradeCppmMultiplicative, long int upgradeRiskFactorAdditive, double upgradeRiskFactorMultiplicative, long int upgradeSellPriceAdditive, double upgradeSellPriceMultiplicative, long int upgradeProductionCostAdditive, double upgradeProductionCostMultiplicative, string upgradeName, long int upgradeCost, string upgradeDescription)
+{
+	upgradeCppmAdditive = 0;
+	upgradeCppmMultiplicative = 0;
+
+	upgradeRiskFactorAdditive = 0;
+	upgradeRiskFactorMultiplicative = 0;
+
+	upgradeSellPriceAdditive = 0;
+	upgradeSellPriceMultiplicative = 0;
+
+	upgradeProductionCostAdditive = 0;
+	upgradeProductionCostMultiplicative = 0;
+
+	upgradeName = "";
+
+	upgradeCost = 0;
+
+	upgradeDescription = "";
+}
+
+long int Upgrade::activeUpgrade(ProductionMethod : Production *)
+{
+	return 0;
+}
+
+void Upgrade::displayUpgradeParameters()
+{
+}
--- CS 172 Final Project/CS 172 Final Project/production.h
+++ CS 172 Final Project/CS 172 Final Project/production.h
@@ -1,1 +1,39 @@
 #pragma once
+
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+class Upgrade {
+
+private:
+	//upgrade modifying values
+	long int upgradeCppmAdditive;
+	double upgradeCppmMultiplicative;
+
+	long int upgradeRiskFactorAdditive;
+	double upgradeRiskFactorMultiplicative;
+
+	long int upgradeSellPriceAdditive;
+	double upgradeSellPriceMultiplicative;
+
+	long int upgradeProductionCostAdditive;
+	double upgradeProductionCostMultiplicative;
+
+	//upgrade parameters
+	string upgradeName;
+	long int upgradeCost;
+	string upgradeDescription;
+
+public:
+	//class definition
+	Upgrade(long int upgradeCppmAdditive, double upgradeCppmMultiplicative, long int upgradeRiskFactorAdditive, double upgradeRiskFactorMultiplicative, long int upgradeSellPriceAdditive, double upgradeSellPriceMultiplicative, long int upgradeProductionCostAdditive, double upgradeProductionCostMultiplicative, string upgradeName, long int upgradeCost, string upgradeDescription);
+
+	//I Don't quite remember what this was supposed to do
+	long int activeUpgrade(ProductionMethod: Production*);
+	
+	//I'm not entirely sure what this is supposed to do
+	//If I remember correctly it was to display the upgrade parameters to the menu
+	void displayUpgradeParameters();
+};
