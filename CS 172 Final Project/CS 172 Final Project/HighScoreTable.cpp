30dc3de817387ff6ffa40d1a855bf2e74bbe9a37
Benjamin Morledge-Hampton <bmorledge-hampton19@my.whitworth.edu>
2016-12-07 17:28:31 -0800

    -Added skeleton for Implementation of HighScoreTable

---
+++ CS 172 Final Project/CS 172 Final Project/HighScoreTable.cpp
@@ -0,0 +1,22 @@
+#include "HighScoreTable.h"
+
+HighScoreTable::HighScoreTable()
+{
+}
+
+bool HighScoreTable::doesScoreQualify(int months)
+{
+	return false;
+}
+
+void HighScoreTable::addScore(int months)
+{
+}
+
+void HighScoreTable::restoreTable()
+{
+}
+
+void HighScoreTable::saveTable()
+{
+}
