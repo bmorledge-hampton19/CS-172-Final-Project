c44c1ff257602af9bc39ff87c7e83be291f31523
Benjamin Morledge-Hampton <bmorledge-hampton19@my.whitworth.edu>
2016-12-07 17:22:30 -0800

    -Created definition for each of the classes derived from Production

--- CS 172 Final Project/CS 172 Final Project/BreedableCookies.h
+++ CS 172 Final Project/CS 172 Final Project/BreedableCookies.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include "production.h"
+#include <string>
+using namespace std;
+
+// A derivative of the Production class.
+//
+class BreedableCookies : protected Production {
+
+public:
+
+	// Basic constructor which sets default values and calls the initializeUpgrades function.
+	//
+	BreedableCookies();
+
+	// Creates all of the upgrade objects for the class.
+	virtual void initializeUpgrades();
+
+	// Checks to see if catastrophic failure occured.
+	//
+	virtual void checkForFailure();
+
+	// Returns the name of the class.
+	virtual string getNameOfProductionType();
+
+};
--- CS 172 Final Project/CS 172 Final Project/Owner.h
+++ CS 172 Final Project/CS 172 Final Project/Owner.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include "production.h"
+#include <string>
+using namespace std;
+
+// A derivative of the Production class.
+//
+class Owner : protected Production {
+
+public:
+
+	// Basic constructor which sets default values and calls the initializeUpgrades function.
+	//
+	Owner();
+
+	// Creates all of the upgrade objects for the class.
+	virtual void initializeUpgrades();
+
+	// Checks to see if catastrophic failure occured.
+	//
+	virtual void checkForFailure();
+
+	// Returns the name of the class.
+	virtual string getNameOfProductionType();
+
+};
