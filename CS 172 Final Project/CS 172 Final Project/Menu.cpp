2e545c3e44ae17109ef7c1e01c806ce427713a76
Benjamin Morledge-Hampton <bmorledge-hampton19@my.whitworth.edu>
2016-12-07 17:27:53 -0800

    -Added skeleton for implementation

---
+++ CS 172 Final Project/CS 172 Final Project/Menu.cpp
@@ -0,0 +1,22 @@
+#include "Menu.h"
+
+Menu::Menu()
+{
+}
+
+void Menu::drawMenu()
+{
+}
+
+bool Menu::interpretUserInput(char input)
+{
+	return false;
+}
+
+void Menu::purchaseProduction(int productionIndex)
+{
+}
+
+void Menu::calculateMonthlyOutcome()
+{
+}
