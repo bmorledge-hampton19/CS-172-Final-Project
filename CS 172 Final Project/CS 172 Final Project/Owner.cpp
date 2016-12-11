e370fcb33df232d45e10df401b0b19996b1ac8bd
Humanphyti <humanphyti@gmail.com>
2016-12-11 13:18:12 -0800

    began filling the Owner production method

--- CS 172 Final Project/CS 172 Final Project/Owner.cpp
+++ CS 172 Final Project/CS 172 Final Project/Owner.cpp
@@ -2,14 +2,22 @@
 
 Owner::Owner()
 {
+    cookiesProduced = 700;
+    cookieSellValue = 1;
+    productionCost = 500;
+    catastrophicFailureChance = 15;
+    initializeUpgrades();
+    initialCost = 0;
 }
 
 void Owner::initializeUpgrades()
 {
+    Upgrade* Upgrade1 = new Upgrade(225, 0, 
 }
 
 string Owner::checkForFailure()
 {
+    int
 }
 
 string Owner::getNameOfProductionType()
