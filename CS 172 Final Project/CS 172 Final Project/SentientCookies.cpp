f97329ad3fae2e6603d93a8f4462f53d63631296
Benjamin Morledge-Hampton <bmorledge-hampton19@my.whitworth.edu>
2016-12-11 12:53:09 -0800

    -Added skeleton for failure method and implemented getProductionName.

--- CS 172 Final Project/CS 172 Final Project/SentientCookies.cpp
+++ CS 172 Final Project/CS 172 Final Project/SentientCookies.cpp
@@ -10,9 +10,20 @@ void SentientCookies::initializeUpgrades()
 
 string SentientCookies::checkForFailure()
 {
+	// Generate a random number to determine whether or not failure occurs.
+	if (rand() % 100 < catastrophicFailureChance) {
+
+		// Incur the effects of the failure.
+
+
+		// Let the user know what happened.
+		return "";
+
+	}
+	else return "";
 }
 
 string SentientCookies::getNameOfProductionType()
 {
-	return string();
+	return "Sentient Cookies";
 }
